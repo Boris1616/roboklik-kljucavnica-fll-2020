3eda9746cf0bda608693e33137005197eb1a85dd
Boris1616 <60433075+Boris1616@users.noreply.github.com>
2020-02-19 14:41:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,45 @@
 radio.onReceivedString(function (receivedString) {
+    // če je koda 11001
     if (receivedString == "11001") {
+        // odkleni
         servos.P0.setAngle(0)
         basic.showLeds(`
             # # # # #
             # . . . #
-            # . . . .
+            # . . # .
             # # # # #
             # # # # #
             `)
+        // ni več zaklenjeno
         zaklenjeno = false
     }
 })
+// ko pritisneš gumb A+B
 input.onButtonPressed(Button.AB, function () {
+    // sprazni zaslon
     basic.clearScreen()
+    // izpiše P I N
     basic.showString("P I N")
     pin = ""
 })
+// ko pritisneš gumb B
 input.onButtonPressed(Button.B, function () {
+    // doda pinu šifro 0
     pin = "" + pin + "0"
+    // pokliči 'ZAKLENI'
     zakleni()
 })
 function zakleni () {
+    // če ima pin pet šifer
     if (pin.length == 5) {
+        // če je šifra enaka prejšni
         if (zaklenjeno == true) {
+            // če je pin za odklenit enak pinu
+            //
+            //
+            // če pin za odklenit ni enak pinu
             if (pin_za_odklenit == pin) {
+                // odkleni
                 servos.P0.setAngle(0)
                 basic.showLeds(`
                     # # # # #
@@ -33,10 +49,13 @@ function zakleni () {
                     # # # # #
                     `)
                 basic.pause(1000)
+                // ni več zaklenjeno
                 zaklenjeno = false
             } else {
+                // pokaži smrtno glavo
                 basic.showIcon(IconNames.Skull)
                 basic.pause(1000)
+                // sprazni zaslon
                 basic.clearScreen()
             }
         } else {
@@ -55,7 +74,9 @@ function zakleni () {
         }
     }
 }
+// ko pritisneš gumb A
 input.onButtonPressed(Button.A, function () {
+    // doda pinu šifro 1
     pin = "" + pin + "1"
     zakleni()
 })
